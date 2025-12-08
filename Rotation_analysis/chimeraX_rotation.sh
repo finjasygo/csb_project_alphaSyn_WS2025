ec23028c48996f6686d48a1c9f83dd47e65c49c1
Finja Sygo <145127464+finjasygo@users.noreply.github.com>
2025-12-09 00:39:02 +0100

    adding script chimeraX_rotation.sh
    bash script for Usage in ChimeraX to analyze the rotation of the structures 8H03, 7XO1, 7XO0, 7XO2, 7XO3, 8H04 and 8H05
    The Order of compared chains is based on the results after executing the script sorting_chains.py

---
+++ Rotation_analysis/chimeraX_rotation.sh
@@ -0,0 +1,154 @@
+#-------------------------------
+###         8H03
+#-------------------------------
+open /Users/finjasygo/Documents/uni_MScBioinfo/sem3/project_csb/structures_from_rcsb_pdb/8H03.pdb
+split #1
+
+match #1.5 to #1.2
+measure rotation #1.2 to #1.6
+match #1.2 to #1.6
+measure rotation #1.2 to #1.6
+match #1.6 to #1.5
+measure rotation #1.6 to #1.5
+
+match #1.3 to #1.1
+measure rotation #1.3 to #1.1
+match #1.1 to #1.4
+measure rotation #1.1 to #1.4
+match #1.4 to #1.3
+measure rotation #1.4 to #1.3
+
+close all
+
+
+#-------------------------------
+####        7XO1
+#-------------------------------
+open /Users/finjasygo/Documents/uni_MScBioinfo/sem3/project_csb/structures_from_rcsb_pdb/7XO1.pdb
+split #1
+
+match #1.5 to #1.4
+measure rotation #1.5 to #1.4
+match #1.4 to #1.6
+measure rotation #1.4 to #1.6
+match #1.6 to #1.5
+measure rotation #1.6 to #1.5
+
+match #1.3 to #1.2
+measure rotation #1.3 to #1.2
+match #1.2 to #1.1
+measure rotation #1.2 to #1.1
+match #1.1 to #1.3
+measure rotation #1.1 to #1.3
+
+close all
+
+
+#-------------------------------
+####        7XO0
+#-------------------------------
+open /Users/finjasygo/Documents/uni_MScBioinfo/sem3/project_csb/structures_from_rcsb_pdb/7XO0.pdb
+split #1
+
+match #1.4 to #1.1
+measure rotation #1.4 to #1.1
+match #1.1 to #1.3
+measure rotation #1.1 to #1.3
+match #1.3 to #1.4
+measure rotation #1.3 to #1.4
+
+match #1.6 to #1.2
+measure rotation #1.6 to #1.2
+match #1.2 to #1.5
+measure rotation #1.2 to #1.5
+match #1.5 to #1.6
+measure rotation #1.5 to #1.6
+
+close all
+
+
+#-------------------------------
+####        7XO2
+#-------------------------------
+open /Users/finjasygo/Documents/uni_MScBioinfo/sem3/project_csb/structures_from_rcsb_pdb/7XO2.pdb
+split #1
+
+match #1.2 to #1.1
+measure rotation #1.2 to #1.1
+match #1.1 to #1.3
+measure rotation #1.1 to #1.3
+match #1.3 to #1.2
+measure rotation #1.3 to #1.2
+
+match #1.5 to #1.4
+measure rotation #1.5 to #1.4
+match #1.4 to #1.6
+measure rotation #1.4 to #1.6
+match #1.6 to #1.5
+measure rotation #1.6 to #1.5
+
+close all
+
+
+#-------------------------------
+####        7XO3
+#-------------------------------
+open /Users/finjasygo/Documents/uni_MScBioinfo/sem3/project_csb/structures_from_rcsb_pdb/7XO3.pdb
+split #1
+
+match #1.6 to #1.2
+measure rotation #1.6 to #1.2
+match #1.2 to #1.5
+measure rotation #1.2 to #1.5
+match #1.5 to #1.6
+measure rotation #1.5 to #1.6
+
+match #1.4 to #1.1
+measure rotation #1.4 to #1.1
+match #1.1 to #1.3
+measure rotation #1.1 to #1.3
+match #1.3 to #1.4
+measure rotation #1.3 to #1.4
+
+close all
+
+
+#-------------------------------
+####        8H04
+#-------------------------------
+open /Users/finjasygo/Documents/uni_MScBioinfo/sem3/project_csb/structures_from_rcsb_pdb/8H04.pdb
+split #1
+
+match #1.4 to #1.1
+measure rotation #1.4 to #1.1
+match #1.1 to #1.3
+measure rotation #1.1 to #1.3
+match #1.3 to #1.4
+measure rotation #1.3 to #1.4
+
+match #1.6 to #1.2
+measure rotation #1.6 to #1.2
+match #1.2 to #1.5
+measure rotation #1.2 to #1.5
+match #1.5 to #1.6
+measure rotation #1.5 to #1.6
+
+close all
+
+
+#-------------------------------
+####        8H05
+#-------------------------------
+open /Users/finjasygo/Documents/uni_MScBioinfo/sem3/project_csb/structures_from_rcsb_pdb/8H05.pdb
+split #1
+
+match #1.2 to #1.1
+measure rotation #1.2 to #1.1
+match #1.1 to #1.3
+measure rotation #1.1 to #1.3
+match #1.3 to #1.2
+measure rotation #1.3 to #1.2
+
+close all
+
+
